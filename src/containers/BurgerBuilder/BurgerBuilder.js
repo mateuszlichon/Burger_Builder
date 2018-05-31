56fbfff1cb66ca9293a4578f06279ac27daaea48
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-31 14:21:45 +0200

    implements removeing ingredients function

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -36,7 +36,19 @@ class BurgerBuilder extends Component {
     };
 
     removeIngredientHandler = (type) => {
-
+        const oldCount = this.state.ingredients[type];
+        if (oldCount <= 0) {
+            return;
+        }
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const updatedPrice = oldPrice - priceDeduction;
+        this.setState({totalPrice : updatedPrice, ingredients : updatedIngredients});
     };
 
     render() {
@@ -44,7 +56,8 @@ class BurgerBuilder extends Component {
             <Auxiliary>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
-                    ingredientAdded={this.addIngredientHandler} />
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler} />
             </Auxiliary>
         );
     }
