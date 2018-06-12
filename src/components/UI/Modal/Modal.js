b9b8ded451e95b88b004d6ca9e7c057c6b143dea
mateuszlichon <mateusz.lichon@gmail.com>
2018-06-12 16:49:07 +0200

    adds close order function on backdrop

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -6,7 +6,9 @@ import Backdrop from '../Backdrop/Backdrop';
 
 const modal = (props) => (
     <Auxiliary>
-        <Backdrop show={props.show} />
+        <Backdrop
+            show={props.show}
+            clicked={props.modalClosed}  />
         <div
             className={classes.Modal}
             style={{
