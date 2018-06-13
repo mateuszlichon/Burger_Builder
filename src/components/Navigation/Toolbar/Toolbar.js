3de80fb4d26373ad40155903c521611fece8c7fd
mateuszlichon <mateusz.lichon@gmail.com>
2018-06-13 11:10:51 +0200

    adds toolbar to main page

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import classes from './Toolbar.css';
+
 const toolbar = () => (
-    <header>
+    <header className={classes.Toolbar}>
         <div>MENU</div>
         <div>LOGO</div>
         <nav>
