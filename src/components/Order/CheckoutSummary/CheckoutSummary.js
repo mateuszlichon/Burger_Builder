9f07e2a2a01a00791ac7eaa675aa2248c7fa2fe6
mateuszlichon <mateusz.lichon@gmail.com>
2018-06-26 15:06:19 +0200

    develops checkout summary

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import Burger from '../../Burger/Burger';
 import Button from '../../UI/Button/Button';
+import classes from './CheckoutSummary.css';
 
-const checkoutSummary = () => {
+const checkoutSummary = (props) => {
     return (
-        <div>
+        <div className={classes.CheckoutSummary}>
             <h1>We hope it tastes well!</h1>
             <div style={{ width: '300px', height: '300px', margin: 'auto' }}>
                 <Burger ingredients={props.ingredients} />
